fcdec465d279aec00ad01fe818fc649160e4f87e
Soham-Lodh <sohamlodh06@gmail.com>
2026-06-10 15:11:35 +0530

    updates

--- backend/main.py
+++ backend/main.py
@@ -7,8 +7,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://your-frontend-domain.vercel.app"
     ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
